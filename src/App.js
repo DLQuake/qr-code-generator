9f1020d595d477ee9f9fbaa2e84e0463c0ab7c50
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-04-01 18:42:15 +0200

    Create first version of qr code generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 function App() {
+  const [text, setText] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code Generator</h1>
+      <div className="form-group">
+        <label htmlFor="input-text">Enter Text or URL:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="input-text"
+          placeholder="Enter text or URL"
+          value={text}
+          onChange={handleInputChange}
+        />
+      </div>
+      {text && (
+        <div className="qr-code">
+          <QRCode value={text} size={256} />
+          <div className="btn-group">
+            <CopyToClipboard text={text} onCopy={handleCopy}>
+              <button className="btn btn-primary">Copy to Clipboard</button>
+            </CopyToClipboard>
+            {copied && <span className="text-success">Copied!</span>}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
